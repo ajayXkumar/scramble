b6ecf9379a4cbec94aa2c9d66ea67b4f16c6f613
ajayXkumar <ajaymatrix386@gmail.com>
2023-10-24 20:21:15 +0530

    chances count added

--- src/screens/scramble.js
+++ src/screens/scramble.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState} from 'react'
+import {Link } from 'react-router-dom'
 import question from "../data"
 import "./scramble.css"
 const Scramble = () => {
@@ -12,11 +13,14 @@ const Scramble = () => {
     const [min ,setmin]=useState(10)
     const [ss ,setss]=useState(0)
     const [pic ,setpic]=useState(0)
+    const [life ,setlife]=useState("/0.png")
+    const [leftt,setleftt]=useState(3);
     const checkans=()=>{
 
        if(ans.toUpperCase()==question[count].a.toUpperCase()){
           setcount(count+1);
           setmarks(marks+1);
+          setleftt(3);
           if(count==question.length-1){
             setcount(0)
             setresult(false);
@@ -25,9 +29,36 @@ const Scramble = () => {
        }
        else{
         setwrong(wrong+1)
+        setleftt(leftt-1)
+        if(wrong==0){
+          setlife("/12.png")
+        }
+        if(wrong==1){
+          setlife("/11.png")
+        }
         if(wrong==2){
             setmarks(marks-1);
+            setcount(count+1)
+            if(count==question.length-1){
+              setcount(0)
+              setresult(false);
+              if(marks>=-30 && marks<=3){
+                setpic("/3.png")
+              }
+              if(marks>=4 && marks<=7){
+                setpic("/7.png")
+              }
+              if(marks>7 && marks<10){
+                setpic("/9.png")
+              }
+              if(marks==10){
+                setpic("/10.png")
+              }
+              
+            }
+            
             setwrong(0);
+            setleftt(3)
         }
        }
     }
@@ -49,6 +80,7 @@ const Scramble = () => {
  
    const handlenext=()=>{
        setcount(count+1)
+       setleftt(3);
        if(count==question.length-1){
         if(marks>=0 && marks<=3){
           setpic("/3.png")
@@ -57,7 +89,7 @@ const Scramble = () => {
           setpic("/7.png")
         }
         if(marks>7 && marks<10){
-          setpic("/7.png")
+          setpic("/9.png")
         }
         if(marks==10){
           setpic("/10.png")
@@ -70,7 +102,13 @@ const Scramble = () => {
     const yourAns=(e)=>{
         setans(e);
     }
-
+    
+    const replay=()=>{
+      setresult(true);
+      setcount(0)
+      setleftt(3);
+      setmarks(0)
+    }
     
     
   return (
@@ -84,12 +122,11 @@ const Scramble = () => {
             
             <p className='marks'>score: {marks}</p>
         </div>
-      
-        
+          
         <div className='question'>   
             <p>{question[count].id}. {question[count].q}</p>
         </div>
- 
+        <p style={{fontSize:"10px",marginLeft:"25px"}}>chances: {leftt}</p>
          <form>
              <input
                 className='user-input'
@@ -107,6 +144,10 @@ const Scramble = () => {
         <div className='result'>
             <img src={pic} style={{width:"80px" , marginTop:"60px",marginLeft:"110px"}}/>
             <h1 style={{textAlign:"center"}}>{marks}</h1>
+
+            <button onClick={replay} style={{background:"none" ,border:"none",marginLeft:"120px",marginTop:"10px"}}><img src="/replay.png" style={{width:"40px" ,cursor:"pointer"}} /> </button>
+              
+            
         </div>
         
       
